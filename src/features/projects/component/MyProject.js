f219190a67273bca76d8f482c951a20d93f04a42
neerajchinwan <neeraj14chinwan@gmail.com>
2024-06-08 21:49:44 +0530

    add new project

--- src/features/projects/component/MyProject.js
+++ src/features/projects/component/MyProject.js
@@ -6,24 +6,31 @@ function MyProject(){
     const projectList = [
         {
             id: 1,
-            image: 'Images/astrix.png',
-            title: '1. Modern Design',
-            description: `This is only the front end part mainly build on react js and tailwind css and this is just a mordern design page.`,
+            image: 'Images/movie.png',
+            title: '1. Movie App',
+            description: `This is complete MERN Project of showing movie and in this you can find  the filter, sorting, pagination and search functionality as of now I use the simple UI but I will change it.`,
             location: "https://teal-souffle-221ae6.netlify.app/"
         },
         {
             id: 2,
+            image: 'Images/astrix.png',
+            title: '2. Modern Design',
+            description: `This is only the front end part mainly build on react js and tailwind css and this is just a mordern design page.`,
+            location: "https://teal-souffle-221ae6.netlify.app/"
+        },
+        {
+            id: 3,
             image: 'Images/todoList.png',
-            title: '2. Todo List',
+            title: '3. Todo List',
             description: `This is the first project full stack project, I used react for front end and many other technologies for frontend like react hook form, axios for fetching the request, redux for state management library, Sweetalert to show the alert messages, bootstrap.
             I used mongodb as a databse and i used mongoose because it is the most famous odm for mongodb
             I used expressjs and nodejs and also used json webtoken for authentication but for now I did not create a session I prefer to store the token in the localStorage for now.`,
             location: "https://todo-list-six-lake.vercel.app/"
         },
         {
-            id: 3,
+            id: 4,
             image: 'Images/flagquiz.png',
-            title: '3. Flag Quiz',
+            title: '4. Flag Quiz',
             description: `This a react practice which I made a simple game of guessing the flag of the countries and you got one point if you guess the right flag`,
             location: 'https://flagquiz-five.vercel.app/'
         }
